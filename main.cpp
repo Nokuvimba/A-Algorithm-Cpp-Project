66cdbdc8d4df6a5c1f5023f02c545c4ee50eb143
NOKUVIMBA CHIYAKA <G00423951@atu.ie>
2026-02-04 15:06:32 +0000

    printing out an int and string matrices.

--- main.cpp
+++ main.cpp
@@ -6,3 +6,17 @@ Start Date: 04/02/2026
 Last Modified Date:
 =========================================
 */
+
+#include "aAlgorithm.h"
+
+#include <iostream>
+
+int main() {
+	AAlgorithm aStar;  // Create an instance of AAlgorithm
+
+	aStar.print(); // Call the print method to display the grid
+
+	NumberMatrix numMatrix; // Create an instance of NumberMatrix
+	numMatrix.print(); // Call the print method to display the matrix
+	return 0;
+}
